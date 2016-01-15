30674f8ff7c6ccddd9fffa85c4761b771f720b82
Popescu Andrei <Popescu Andrei>
2016-01-15 20:16:12 +0200

    added admin controller and notifications in login pages

---
+++ src/main/java/ro/fils/highschoolplatform/controller/AdminController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ro.fils.highschoolplatform.controller;
+
+import java.util.ArrayList;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import ro.fils.highschoolplatform.domain.Student;
+import ro.fils.highschoolplatform.util.Encryption;
+
+/**
+ *
+ * @author andre
+ */
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody
+    Boolean loginUser(@RequestParam("mail") String mail, @RequestParam("password") String password) {
+        if (mail.equals("admin") && password.equals("admin")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
